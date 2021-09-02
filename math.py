35bf1531276740a7debca37cec504d606d455b93
piyaldey123 <90005461+piyaldey123@users.noreply.github.com>
2021-09-03 01:57:27 +0530

    initial commit of math.py

---
+++ math.py
@@ -0,0 +1,16 @@
+#add implementation
+def add(x,y):
+    pass
+    
+#subtract implementation
+def subtract(x,y):
+    pass
+    
+#multiply implementation
+def multiply(x,y):
+    pass
+    
+#divide implementation
+def divide(x,y):
+    pass
+
